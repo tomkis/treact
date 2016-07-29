579cc80465a95476eed8b127da2828b5c1d27b2d
Tomas Weiss <tomasw@salsitasoft.com>
2016-07-29 15:30:30 +0200

    implementing hyperscriptish element & function to build the VDOM

--- example/main.js
+++ example/main.js
@@ -1,2 +1,19 @@
-import hello from '../src/index';
-hello();
+import { h, createRender } from '../src/index';
+
+const View = ({ greeted }) => {
+  if (greeted) {
+    return h('p', {}, [
+      h('text', { text: 'Hello World!' })
+    ]);
+  } else {
+    return h('button', {}, [
+      h('text', { text: 'Say Hi!' })
+    ]);
+  }
+};
+
+const render = createRender(document.getElementById('app'));
+
+render(h(View, {
+  greeted: false
+}));
--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
-export default () => console.log('Hello World');
+/**
+ * Creates HyperScriptish Element
+ *
+ * @param {string|function} Type of the Element,
+ *        this may either be String (for DOM nodes) or Function for Components
+ *
+ * @param {object} Props of the Element/DOM Node
+ * @param {array} List of child Elements
+ *
+ * @returns {object} Object abstraction of the Element
+ */
+export const h = (type, props = {}, children = []) => ({
+  type,
+  props,
+  children
+});
+
+/**
+ * Recursively creates VDOM representation from
+ * root Element. It goes through the Element tree
+ * and executes all the Components (type is function)
+ * while passing provided props as Component argument.
+ *
+ * @param {object} HyperScriptish Element
+ * @return {object} VDOM tree
+ */
+const createVDOM = element => {
+  // Let's call this function recursively over all the
+  // Children
+  const newElement = {
+    ...element,
+    children: element.children.map(child => createVDOM(child))
+  };
+
+  // Is this Element Component?
+  if (typeof element.type === 'function') {
+    // Execute the function and provide appropriate
+    // properties as the function argument
+    return createVDOM(newElement.type(element.props));
+  } else {
+    // Plain DOM node can be returned untouched
+    return newElement;
+  }
+};
+
+export const createRender = domElement => {
+  let lastVDOM = null;
+
+  return element => {
+    const vdom = createVDOM(element);
+    console.log(vdom);
+
+    lastVDOM = vdom;
+  };
+};
